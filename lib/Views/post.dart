9b615a7dc853b0ee6e2e5ede22f486cd72bfd1ba
tsauve <tsauve@umich.edu>
2020-09-02 22:45:51 -0400

    Added ideas and removed commented code

--- lib/Views/post.dart
+++ lib/Views/post.dart
@@ -2,39 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:social_media/Models/video.dart';
 import 'package:social_media/Models/profile.dart';
 
-class Post extends StatelessWidget{
+class Post extends StatelessWidget {
   Post({this.video});
 
   final Video video;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      child:Row(children: <Widget>[
-        Image(
-          image: video.thumbnail,
-          width: 150
-        ),
-        SizedBox(width: 50,),
-        Column(
-          children: <Widget>[
-            Text(video.videoTitle),
-            SizedBox(height: 10,),
-            Row(children: <Widget>[
-              CircleAvatar(backgroundImage: video.profile.profilePicture,),
-              SizedBox(width: 10),
-              Text(video.profile.profileName),
-            ],),
-            SizedBox(height: 10,),
-            Text(video.getViewCount() + " views"),
-            SizedBox(height: 10,),
-            Text("Video posted yesterday"),
-          ],
-        )  
-      ]
-      )
-    );
+        margin: EdgeInsets.only(bottom: 10),
+        child: Row(children: <Widget>[
+          Image(image: video.thumbnail, width: 150),
+          SizedBox(
+            width: 50,
+          ),
+          Column(
+            children: <Widget>[
+              Text(video.videoTitle),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: video.profile.profilePicture,
+                  ),
+                  SizedBox(width: 10),
+                  Text(video.profile.profileName),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(video.getViewCount() + " views"),
+              SizedBox(
+                height: 10,
+              ),
+              Text("Video posted yesterday"),
+            ],
+          )
+        ]));
   }
-
 }
